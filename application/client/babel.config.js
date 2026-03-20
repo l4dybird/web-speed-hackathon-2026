44b7e487b4a6872aa02c15d9024f1073b38b8c33
LADYBIRD <57829584+l4dybird@users.noreply.github.com>
2026-03-20 13:21:00 +0900

    refactor: update babel configuration to target the latest Chrome version and enable bugfixes

--- application/client/babel.config.js
+++ application/client/babel.config.js
@@ -4,10 +4,9 @@ module.exports = {
     [
       "@babel/preset-env",
       {
-        targets: "ie 11",
-        corejs: "3",
+        bugfixes: true,
+        targets: "last 1 Chrome version",
         modules: false,
-        useBuiltIns: false,
       },
     ],
     [
